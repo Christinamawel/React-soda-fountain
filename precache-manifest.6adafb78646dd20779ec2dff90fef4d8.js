77d2cdc74b145af22e3c35aa0e65bb072de64066
Christinamawel <christinamawel@gmail.com>
2022-03-14 15:27:23 -0700

    Updates

---
+++ precache-manifest.6adafb78646dd20779ec2dff90fef4d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7a8fbe7be841b4f540e8ca4f7ec3c9",
+    "url": "/React-soda-fountain/index.html"
+  },
+  {
+    "revision": "1c657b8d90233f3b0f59",
+    "url": "/React-soda-fountain/static/css/main.f32f0a9a.chunk.css"
+  },
+  {
+    "revision": "044a6ab4216b545f80c0",
+    "url": "/React-soda-fountain/static/js/2.8870e625.chunk.js"
+  },
+  {
+    "revision": "a8fba7eb98a92a60c75c",
+    "url": "/React-soda-fountain/static/js/3.87562ae1.chunk.js"
+  },
+  {
+    "revision": "1c657b8d90233f3b0f59",
+    "url": "/React-soda-fountain/static/js/main.90a7967a.chunk.js"
+  },
+  {
+    "revision": "bd6a047dc6c5a9487061",
+    "url": "/React-soda-fountain/static/js/runtime-main.2d39e13a.js"
+  },
+  {
+    "revision": "1f5600cb8dc5bd33186989d48bf039cc",
+    "url": "/React-soda-fountain/static/media/diner-photo-Spencer-Davis.1f5600cb.jpg"
+  }
+]);
